e326fa5f8429afc95d133387057b7209af8a013e
AgustinGalante19 <agustin.galante.19@outlook.es>
2023-04-13 21:26:35 -0300

    ejercicio terminado

--- Clase 1/js/funciones.js
+++ Clase 1/js/funciones.js
@@ -1,1 +1,34 @@
-console.log("hola mundo")
+const btnEnviar = document.getElementById("btnEnviar")
+const nombre = document.getElementById("nombre");
+
+/* 
+
+Forma 1
+let txt = ""
+
+nombre.addEventListener("change", (e) => {
+    txt = e.target.value
+})
+
+btnEnviar.addEventListener("click", (e) => {
+    e.preventDefault()
+    console.log(txt)
+})
+
+*/
+
+/*
+Forma 2
+const frmNombre = document.getElementById("formNombre")
+
+frmNombre.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const fields = new FormData(e.target)
+    const nombre = fields.get("nombre")
+    console.log(nombre)
+}) */
+
+/* Forma 3 */
+function mostrarNombre() {
+    console.log(nombre.value)
+}
