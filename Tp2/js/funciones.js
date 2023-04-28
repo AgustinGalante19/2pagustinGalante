c440d67a90acaedf35bf7c004673553aed99c2b6
Agustin Galante <73099311+AgustinGalante19@users.noreply.github.com>
2023-04-28 12:30:53 -0300

    TP 2 terminado

---
+++ Tp2/js/funciones.js
@@ -0,0 +1,54 @@
+const id = document.getElementById("id");
+const nombre = document.getElementById("nombre");
+const estado = document.getElementById("estado");
+const episodios = document.getElementById("episodios");
+const imagen = document.getElementById("imagen");
+
+const getCharacter = async () => {
+  const request = await fetch(
+    `https://rickandmortyapi.com/api/character/${id.value}`
+  );
+  const character = await request.json();
+
+  const episodes = character.episode;
+  const episodesId = episodes.map((episodio) => {
+    let aux = episodio.split("/");
+    let id = aux[aux.length - 1];
+    return id;
+  });
+
+  const ids = episodesId.toString();
+
+  const requestEpisodes = await fetch(
+    "https://rickandmortyapi.com/api/episode/" + ids
+  );
+
+  const episodesResponse = await requestEpisodes.json();
+  console.log(episodesResponse);
+  nombre.textContent = character.name;
+  estado.textContent = character.status;
+  especie.textContent = character.species;
+  estado.textContent = character.status;
+  if (episodesResponse.length > 0) {
+    const elementos = episodesResponse
+      .map(
+        (ep) => `<li>
+      <h4>${ep.name} - ${ep.episode}</h4>
+      </li>`
+      )
+      .join("");
+
+    episodios.innerHTML = elementos;
+  } else {
+    episodios.innerHTML =
+      episodesResponse.name + " - " + episodesResponse.episode;
+  }
+  imagen.src = character.image;
+  if (character.status === "Alive") {
+    estado.classList = "alive";
+  } else if (character.status === "Dead") {
+    estado.classList = "dead";
+  } else {
+    estado.classList = "unknown";
+  }
+};
